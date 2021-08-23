1067fbf62a741275a092db47c9d1ee13fb0d58b8
Sean Keever <swkeever@gmailcom>
2021-08-22 20:09:16 -0700

    update env var names

--- lib/ssg.ts
+++ lib/ssg.ts
@@ -2,10 +2,10 @@ import {GetStaticProps} from "next";
 import {createClient} from "@supabase/supabase-js";
 import {LayoutProps} from "../components/layout";
 
-export const getArticleProps = async (path: string): Promise<ReturnType<GetStaticProps>> => {
+export const getArticleProps = async (path: string): Promise<ReturnType<GetStaticProps<LayoutProps>>> => {
     const supabase = createClient(
-        <string>process.env.NEXT_PUBLIC_SUPABASE_URL,
-        <string>process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+        <string>process.env.SUPABASE_URL,
+        <string>process.env.SUPABASE_ANON_KEY
     )
 
     const {data, error} = await supabase
